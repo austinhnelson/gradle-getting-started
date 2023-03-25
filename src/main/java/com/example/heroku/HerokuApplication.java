a3df2acee7c51e57b94459ad03ebf5f5be68b9a8
Austin Nelson <nelsonhaustin@gmail.com>
2023-03-24 23:02:28 -0600

    Commit 2

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -73,13 +73,12 @@ public class HerokuApplication {
       Statement stmt = connection.createStatement();
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(30))");
       stmt.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM table_timestamp_and_random_string");
-      ResultSet rs2 = stmt.executeQuery("SELECT random_string FROM table_timestamp_and_random_string");
-      rs2.next();
+      ResultSet rs = stmt.executeQuery("SELECT tick, random_string FROM table_timestamp_and_random_string");
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick") + rs2.toString());
+        String rand_string = rs.getString("random_string");
+        output.add("Read from DB: " + rs.getTimestamp("tick") + " " + rand_string);
       }
 
       model.put("records", output);
