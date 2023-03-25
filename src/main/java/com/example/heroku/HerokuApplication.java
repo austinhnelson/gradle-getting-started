f151308c56dc370ff10db91537b468ba18a2db16
Austin Nelson <nelsonhaustin@gmail.com>
2023-03-24 23:08:21 -0600

    Commit 1

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -69,6 +69,7 @@ public class HerokuApplication {
 
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
+    System.out.println("Austin Nelson");
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(30))");
